607912b568da2cd1759649995092bf1e3ca0ccc6
bytil70 <bytil70@gmail.com>
2025-04-10 17:04:00 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+
+//variable = A container that stores a value.
+//           Behaves as if it  were the value it  contains.
+
+//1. declaration let x;
+//2. assignment  x = 100;
+
+/*let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(typeof price);
+console.log(`You are ${age} years old.`);
+console.log(`The price is $${price}.`)
+console.log(`Your gpa is ${gpa}.`);
+*/
+
+/*let firstName = "Burn";
+let favoriteFood = 'pizza'
+let email = "burnacc@gamil.com"
+
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`My favorite food is ${favoriteFood}`)
+console.log(`your email is ${email}`)
+*/
+
+/*let online = false;
+let forsale = true;
+let isStudent = true;
+
+console.log(`Burn is online: ${online}`);
+console.log(`Is the car for sale? ${forsale}`);
+console.log(`Enrolled? ${isStudent}`);
+*/
+
+let fullName= "Burn Lai";
+let age = 65;
+let isStudent = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}.`;
+document.getElementById("p2").textContent = `You are ${age} years old.`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
+
+
+
+
+
+
+
+
+
+
